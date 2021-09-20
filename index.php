549db3baee0fdc27391322044fc20d54dd9c622d
k-dimitrijevs <kristians.dimitrijevs@gmail.com>
2021-09-20 16:00:00 +0300

    Added option to select from multiple calendars

--- index.php
+++ index.php
@@ -6,18 +6,27 @@ require_once 'vendor/autoload.php';
 
 if (isset($_POST['submit']))
 {
+    $calendar = $_POST['calendars'];
     $from = DateTime::createFromFormat('Y-m-d H:i', $_POST['from']);
     $to = DateTime::createFromFormat('Y-m-d H:i', $_POST['to']);
 
     $link = Link::create($_POST['title'], $from, $to)
         ->description($_POST['desc']);
 
-    echo "<a href='" . $link->google() . "' target='_blank'>CLICK HERE</a>";
+    echo "<a href='" . $link->$calendar() . "' target='_blank'>CLICK HERE</a>";
 }
 
 ?>
 
 <form method="post">
+    <br><label for="calendars">Choose a calendar:</label><br>
+    <select id="calendars" name="calendars">
+        <option value="google">Google</option>
+        <option value="yahoo">Yahoo</option>
+        <option value="webOutlook">OutLook</option>
+        <option value="ics">ICS</option>
+    </select><br><br>
+
     <label for="title">Title:</label><br>
     <input type="text" id="title" name="title"><br>
 
